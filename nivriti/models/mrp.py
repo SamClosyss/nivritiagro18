e3397716e6034741abf83275ff29624fa2c17960
Shb-Closyss <shubham@closyss.com>
2026-02-02 16:04:24 +0530

    Commented code in mo.production

--- nivriti/models/mrp.py
+++ nivriti/models/mrp.py
@@ -10,3 +10,12 @@ class MrpProductionInherit(models.Model):
         if self.lot_producing_id:
             self.lot_producing_id.manufacturing_date = date.today()
         return res
+
+    @api.onchange('product_id', 'move_raw_ids', 'never_product_template_attribute_value_ids')
+    def _onchange_product_id(self):
+        for move in self.move_raw_ids:
+            if self.product_id == move.product_id:
+                message = _("The component %s should not be the same as the product to produce.",
+                            self.product_id.display_name)
+                self.move_raw_ids = self.move_raw_ids - move
+                return {'warning': {'title': _('Warning'), 'message': message}}
